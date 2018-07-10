4bc62bc4439351d12450eb836013d7687c3a6d5d
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-07-10 13:49:34 -0500

    Add config file for bright-fatter processing

---
+++ config/bf.py
@@ -0,0 +1,20 @@
+config.isr.doFlat = False  # mandatory for this task
+
+# to be re-examined once more calibration products are available
+config.isr.doBias = True
+config.isr.doDark = True
+config.isr.doFringe = False
+config.isr.doLinearize = False
+config.isr.doDefect = False
+
+config.isr.doWrite = False
+
+config.isr.overscanFitType = "AKIMA_SPLINE"
+config.isr.overscanOrder = 30
+
+config.isr.doAddDistortionModel = False
+config.isr.doUseOpticsTransmission = False
+config.isr.doUseFilterTransmission = False
+config.isr.doUseSensorTransmission = False
+config.isr.doUseAtmosphereTransmission = False
+config.isr.doAttachTransmissionCurve = False
