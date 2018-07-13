6f88c8b003e13e9f5b91a1c811efdcf2fd4e3064
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-07-13 07:02:01 -0500

    Update ccdKey config for singleFrameDriver.py

--- config/singleFrameDriver.py
+++ config/singleFrameDriver.py
@@ -3,4 +3,4 @@ import os.path
 from lsst.utils import getPackageDir
 
 config.processCcd.load(os.path.join(getPackageDir("obs_lsstCam"), "config", "processCcd.py"))
-config.ccdKey = 'ccd'
+config.ccdKey = 'detector'
