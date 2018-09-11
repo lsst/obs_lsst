e2214a4ed9a0f86db93cc5c2d9c4f340de3e8a2f
Robert Lupton the Good <rhl@astro.princeton.edu>
2018-09-11 17:16:07 -0500

    Made various generalisations/cleanups
    Changed default for --dstype to "calexp"
    
    Added error checking for when there's no valid data
    
    Added -c allowFitsCompression=True to control lsst.afw.fits.setAllowImageCompression
    
    Added support for e.g.
      --id visit1^visit2
    
    Added e.g.
      --id visit raftName=RXX
    but didn't change the output template, so it'll overwrite the whole-focal-plane version
    even if you only ask for a subset; but see `-c fpId=XXX`
    
    Support e.g. -c fpId=R22 to rename focal_plane files to e.g.
    	focal_plane/eimage-R22-v00227891.fits
    Useful when including e.g. a raftName in the `--id`
    
    Added config parameter
      doSensorImages
    (default True) to control writing the per-CCD images
    
    Changed filename to e.g.
    	binned_sensor/calexp-v00227891/R22/S20.fits
    	binned_sensor/eimage-v00227891/R22/S20.fits
    	binned_sensor/eimage-v00227891/R22/S20_A.fits
    	binned_sensor/eimage-v00227891/R22/S20_B.fits
    	focal_plane/calexp-v00227891.fits
    	focal_plane/eimage-v00227891.fits

--- bin.src/makeFpSummary.py
+++ bin.src/makeFpSummary.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
-from lsst.obs.lsstCam import SimButlerImage
 from lsst.afw.cameraGeom import utils as cgu
 from lsst.afw.display.rgb import ZScaleMapping, writeRGB
 from lsst.afw.math import rotateImageBy90, flipImage
-import lsst.afw.image as afwImage
 import lsst.afw.geom as afwGeom
 import lsst.afw.fits
 lsst.afw.fits.setAllowImageCompression(False)
+
 
 class ButlerListRunner(pipeBase.TaskRunner):
     """A task runner that calls run with a list of data references
@@ -20,7 +19,7 @@ class ButlerListRunner(pipeBase.TaskRunner):
     def getTargetList(parsedCmd):
         """Return a list of targets (arguments for __call__); one entry per invocation
         """
-        return [(parsedCmd.id.refList, parsedCmd.butler),]  # two arguments consisting of a list of dataRefs and a butler
+        return [(parsedCmd.id.refList, parsedCmd.butler)]  # two arguments; a list of dataRefs and a butler
 
     def __call__(self, args):
         """Run task.runDataRef on a single target
@@ -38,16 +37,27 @@ class ButlerListRunner(pipeBase.TaskRunner):
 
         if self.doReturnResults:
             return pipeBase.Struct(
+                exitStatus=result.exitStatus,
                 dataRefList=dataRefList,
                 metadata=task.metadata,
                 result=result,
             )
+        else:
+            return pipeBase.Struct(
+                exitStatus=result.exitStatus,
+            )
+
 
 class FocalplaneSummaryConfig(pexConfig.Config):
-    binSize = pexConfig.Field(dtype=int, default=50, doc="pixels to bin for the focalplane summary")
+    binSize = pexConfig.Field(dtype=int, default=64, doc="pixels to bin for the focalplane summary")
     contrast = pexConfig.Field(dtype=float, default=1, doc="contrast factor")
     sensorBinSize = pexConfig.Field(dtype=int, default=4, doc="pixels to bin per sensor")
     putFullSensors = pexConfig.Field(dtype=bool, default=False, doc="persist the full size binned sensor?")
+    doSensorImages = pexConfig.Field(dtype=bool, default=True, doc="Make images of the individual sensors")
+
+    def validate(self):
+        if self.putFullSensors and not self.doSensorImages:
+            raise ValueError("You may not set putFullSensors == True if doSensorImages == False")
 
 
 class FocalplaneSummaryTask(pipeBase.CmdLineTask):
@@ -61,44 +71,74 @@ class FocalplaneSummaryTask(pipeBase.CmdLineTask):
     def runDataRef(self, expRefList, butler):
         """Make summary plots of full focalplane images.
         """
-        sbi = SimButlerImage(butler, type=expRefList[0].butlerSubset.datasetType, visit=expRefList[0].dataId['visit'])
+        if len(expRefList) == 0:
+            return pipeBase.Struct(exitStatus=1)
 
-        for expRef in expRefList:
-            data_id = expRef.dataId
-            ccd = butler.get('calexp_detector', **data_id)
-            try:
-                binned_im = sbi.getCcdImage(ccd, binSize=self.config.sensorBinSize, as_masked_image=True)[0]
-                binned_im = rotateImageBy90(binned_im, ccd.getOrientation().getNQuarter())
-                if self.config.putFullSensors:
-                    butler.put(binned_im, 'binned_sensor_fits', **data_id)
-            except (TypeError, RuntimeError):
-                # butler couldn't put the image or there was no image to put
-                continue
+        dstype = expRefList[0].butlerSubset.datasetType
 
-            (x, y) = binned_im.getDimensions()
-            boxes = {'A': afwGeom.Box2I(afwGeom.PointI(0, y/2), afwGeom.ExtentI(x, y/2)),
-                     'B': afwGeom.Box2I(afwGeom.PointI(0, 0), afwGeom.ExtentI(x, y/2))}
-            for half in ('A', 'B'):
-                box = boxes[half]
-                butler.put(afwImage.MaskedImageF(binned_im, box), 'binned_sensor_fits_halves', half=half,
-                           **data_id)
+        if dstype == "raw":
+            def callback(im, ccd, imageSource):
+                return cgu.rawCallback(im, ccd, imageSource, correctGain=True, subtractBias=True)
+        elif dstype == "eimage":
+            callback = eimageCallback
+        else:
+            callback = None
 
-        im = cgu.showCamera(butler.get('camera'), imageSource=sbi, binSize=self.config.binSize)
-        expRef.put(im, 'focalplane_summary_fits')
-        im = flipImage(im, False, True)
-        zmap = ZScaleMapping(im, contrast=self.config.contrast)
-        rgb = zmap.makeRgbImage(im, im, im)
-        file_name = expRef.get('focalplane_summary_png_filename')
-        writeRGB(file_name[0], rgb)
+        for visit in set([er.dataId["visit"] for er in expRefList]):
+            self.log.info("Processing visit %d", visit)
+            expRefListForVisit = [er for er in expRefList if er.dataId["visit"] == visit]
+
+            dataId = expRefListForVisit[0].dataId
+            bi = cgu.ButlerImage(butler, dstype, visit=visit, callback=callback, verbose=True)
+
+            if self.config.doSensorImages:
+                for dataId in (er.dataId for er in expRefListForVisit):
+                    ccd = butler.get('calexp_detector', **dataId)
+                    try:
+                        binned_im = bi.getCcdImage(ccd, binSize=self.config.sensorBinSize)[0]
+                        binned_im = rotateImageBy90(binned_im, ccd.getOrientation().getNQuarter())
+                        if self.config.putFullSensors:
+                            butler.put(binned_im, 'binned_sensor_fits', **dataId)
+                    except (TypeError, RuntimeError) as e:
+                        # butler couldn't put the image or there was no image to put
+                        self.log.warn("Unable to make binned image: %s", e)
+                        continue
+
+                    (x, y) = binned_im.getDimensions()
+                    boxes = {'A': afwGeom.Box2I(afwGeom.PointI(0, y/2), afwGeom.ExtentI(x, y/2)),
+                             'B': afwGeom.Box2I(afwGeom.PointI(0, 0), afwGeom.ExtentI(x, y/2))}
+                    for half in ('A', 'B'):
+                        box = boxes[half]
+                        butler.put(binned_im[box], 'binned_sensor_fits_halves', half=half, **dataId)
+
+            detectorNameList = ["%s_%s" % (er.dataId["raftName"], er.dataId["detectorName"])
+                                for er in expRefListForVisit]
+
+            im = cgu.showCamera(butler.get('camera'), imageSource=bi, binSize=self.config.binSize,
+                                detectorNameList=detectorNameList)
+
+            # filter is needed for butler.put() (but may not be available for eimages)
+            if "filter" in dataId:
+                filter = dataId["filter"]
+            else:
+                filter = butler.queryMetadata("raw", ["filter"], visit=visit)[0]
+
+            butler.put(im, 'focalplane_summary_fits', visit=visit, filter=filter)
+            zmap = ZScaleMapping(im, contrast=self.config.contrast)
+            rgb = zmap.makeRgbImage(im, im, im)
+            file_name = butler.get('focalplane_summary_png_filename', visit=visit, filter=filter)
+            writeRGB(file_name[0], rgb)
+
+        return pipeBase.Struct(exitStatus=0)
 
     @classmethod
     def _makeArgumentParser(cls, *args, **kwargs):
         # Pop doBatch keyword before passing it along to the argument parser
         kwargs.pop("doBatch", False)
 
-        dstype = pipeBase.DatasetArgument('--dstype', default='eimage',
+        dstype = pipeBase.DatasetArgument('--dstype', default='calexp',
                                           help="dataset type to process from input data repository"
-                                               "(i.e., 'eimage', 'calexp')")
+                                               "(e.g. eimage, postISRCCD, or calexp)")
 
         parser = pipeBase.ArgumentParser(name="focalplaneSummary",
                                          *args, **kwargs)
@@ -111,6 +151,18 @@ class FocalplaneSummaryTask(pipeBase.CmdLineTask):
 
     def _getMetadataName(self):
         return None
+
+
+def eimageCallback(im, ccd=None, imageSource=None):
+    """A callback to handle eimages"""
+
+    im = im.convertF()
+
+    nQuarter = 1
+    im = rotateImageBy90(im, nQuarter)
+    im = flipImage(im, True, False)
+
+    return im
 
 
 if __name__ == "__main__":
