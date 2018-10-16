51bada361bedceccc3dd9903470a3b4cdadb120e
Hsin-Fang Chiang <hchiang2@illinois.edu>
2018-10-16 15:30:40 -0500

    Change patchId convention from 1,2 to 1_2

--- python/lsst/obs/lsst/lsstCamMapper.py
+++ python/lsst/obs/lsst/lsstCamMapper.py
@@ -379,7 +379,7 @@ class LsstCamMapper(CameraMapper):
         tract = int(dataId['tract'])
         if tract < 0 or tract >= 2**LsstCamMapper._nbit_tract:
             raise RuntimeError('tract not in range [0,%d)' % (2**LsstCamMapper._nbit_tract))
-        patchX, patchY = [int(patch) for patch in dataId['patch'].split(',')]
+        patchX, patchY = [int(patch) for patch in dataId['patch'].split('_')]
         for p in (patchX, patchY):
             if p < 0 or p >= 2**LsstCamMapper._nbit_patch:
                 raise RuntimeError('patch component not in range [0, %d)' % 2**LsstCamMapper._nbit_patch)
