413f21a6c42687d8b80951e751ff4f0b91a5e947
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-10-24 11:04:01 -0500

    Change dictionary configuration to be explicit
    Previously some keys were the same, others missing, others added
    vs the dicts inheritted. This change is a no-op to the output
    but makes the changes vs the main camera explicit.
    
    Future commits will actually change the ingest.

--- config/ts8/ingest.py
+++ config/ts8/ingest.py
@@ -2,46 +2,31 @@ from lsst.obs.lsst.ts8 import Ts8ParseTask
 
 config.parse.retarget(Ts8ParseTask)
 
-config.parse.translation = {
-    'date': 'DATE-OBS',
-    'dateObs': 'DATE-OBS',
-    'lsstSerial': 'LSST_NUM',
-    'expTime': 'EXPTIME',
-    'imageType': 'IMGTYPE',
-    'object': 'OBJECT',
-    'run': 'RUNNUM',
-    'seqNum': 'SEQNUM',
-    'wavelength': 'MONOWL',
-}
-config.parse.translators = {
-    'dayObs': 'translate_dayObs',
-    'detector': 'translate_detector',
-    'detectorName': 'translate_detectorName',
-    'filter' : 'translate_filter',
-    'visit': 'translate_visit',
-    'wavelength': 'translate_wavelength',
-}
-config.parse.defaults = {
-    'object': "UNKNOWN",
-    'imageType': "UNKNOWN",
-}
+# what used to be config.parse.translation = {
+del config.parse.translation['testType']
+del config.parse.translation['filter']
+del config.parse.translation['visit']
+config.parse.translation['seqNum'] = 'SEQNUM'
+config.parse.translation['wavelength'] = 'MONOWL'
+
+# what used to be config.parse.translators = {
+del config.parse.translators['raftName']
+del config.parse.translators['snap']
+config.parse.translators['dayObs'] = 'translate_dayObs'
+config.parse.translators['filter'] = 'translate_filter'
+config.parse.translators['visit'] = 'translate_visit'
+
+# what used to be config.parse.defaults = {
+config.parse.defaults['imageType'] = "UNKNOWN"
+
 config.parse.hdu = 0
 
-config.register.columns = {
-    'run': 'text',
-    'date': 'text',
-    'dateObs': 'text',
-    'dayObs': 'text',
-    'detector': 'int',
-    'detectorName': 'text',
-    'expTime': 'double',
-    'filter': 'text',
-    'imageType': 'text',
-    'lsstSerial': 'text',
-    'object': 'text',
-    'seqNum': 'int',
-    'visit': 'int',
-    'wavelength': 'int',
-}
+# what used to be config.register.columns = {
+del config.register.columns['raftName']
+del config.register.columns['snap']
+del config.register.columns['testType']
+config.register.columns['dayObs'] = 'text'
+config.register.columns['seqNum'] = 'int'
+
 config.register.unique = ["visit", "detector"]
 config.register.visit = list(config.register.columns.keys())
