4bf3748af1d7a641efecf12e6af78b40ded5fc4c
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-10-24 11:23:42 -0500

    Make the time at which days rollover defined in one place
    This is used in other places, and shouldn't be hard-coded.
    Move it to being a global and add to __all__

--- python/lsst/obs/lsst/ingest.py
+++ python/lsst/obs/lsst/ingest.py
@@ -7,8 +7,11 @@ from . import LsstCam
 
 EXTENSIONS = ["fits", "gz", "fz"]  # Filename extensions to strip off
 
-__all__ = ["LsstCamParseTask"]
+# This is needed elsewhere and should only be defined in one place
+# roll over at 8am UTC
+ROLLOVERTIME = datetime.timedelta(hours=8)
 
+__all__ = ["LsstCamParseTask", "ROLLOVERTIME"]
 
 class LsstCamParseTask(ParseTask):
     """Parser suitable for lsstCam data.
@@ -94,7 +97,7 @@ class LsstCamParseTask(ParseTask):
         dateObs = self.__fixDateObs(md.get("DATE-OBS"))
 
         d = datetime.datetime.strptime(dateObs + "+0000", "%Y-%m-%dT%H:%M:%S.%f%z")
-        d -= datetime.timedelta(hours=8)  # roll over at 8am UTC
+        d -= ROLLOVERTIME
         dayObs = d.strftime("%Y-%m-%d")
 
         return dayObs
