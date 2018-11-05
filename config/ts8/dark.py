133cfe1fca047befc191df64c57447eab578aad5
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-11-05 11:59:03 -0600

    Create (and fix existing) config files for calib construction

--- config/ts8/dark.py
+++ config/ts8/dark.py
@@ -2,3 +2,5 @@ import os.path
 from lsst.utils import getPackageDir
 
 config.load(os.path.join(getPackageDir("obs_lsst"), "config", "ts8", "ts8.py"))
+
+config.repair.cosmicray.nCrPixelMax = 1000000
