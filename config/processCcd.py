4826aa2620a941d85ccb74ed4ca0f7f12167a2cb
Jim Chiang <jchiang@slac.stanford.edu>
2018-12-06 16:59:03 -0800

    changes needed to run with a stars-only reference catalog

--- config/processCcd.py
+++ config/processCcd.py
@@ -61,7 +61,7 @@ for refObjLoader in (config.charImage.refObjLoader,
                      config.calibrate.astromRefObjLoader,
                      config.calibrate.photoRefObjLoader):
     refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
-    refObjLoader.load(os.path.join(getPackageDir('obs_lsst'), 'config', 'filterMap.py'))
+#    refObjLoader.load(os.path.join(getPackageDir('obs_lsst'), 'config', 'filterMap.py'))
     refObjLoader.ref_dataset_name='cal_ref_cat'
 
 # Better astrometry matching
@@ -79,7 +79,8 @@ config.calibrate.photoCal.applyColorTerms = False
 #colors["g-r"] = ColorLimit(primary="g_flux", secondary="r_flux", minimum=0.0)
 #colors["r-i"] = ColorLimit(primary="r_flux", secondary="i_flux", maximum=0.5)
 config.calibrate.photoCal.match.referenceSelection.doMagLimit = True
-config.calibrate.photoCal.match.referenceSelection.magLimit.fluxField = "lsst_i_smeared_flux"
+#config.calibrate.photoCal.match.referenceSelection.magLimit.fluxField = "lsst_i_smeared_flux"
+config.calibrate.photoCal.match.referenceSelection.magLimit.fluxField = "i_flux"
 config.calibrate.photoCal.match.referenceSelection.magLimit.maximum = 22.0
 # select only stars for photometry calibration
 config.calibrate.photoCal.match.sourceSelection.unresolved.maximum=0.5
