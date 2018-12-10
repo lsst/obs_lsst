aaf58ec951a40dd1bc64db3adcaf9ca5e2955910
Heather Kelly <heather@slac.stanford.edu>
2018-12-10 06:27:27 -0800

    removing config/phosim/dark.py

--- config/phosim/dark.py
+++
@@ -1,7 +0,0 @@
-import os.path
-from lsst.utils import getPackageDir
-
-config.load(os.path.join(getPackageDir("obs_lsst"), "config", "lsstCam.py"))
-
-# avoids dark product building failure
-config.repair.cosmicray.nCrPixelMax = 100000000
