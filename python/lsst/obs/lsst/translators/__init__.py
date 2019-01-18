a591113071478d5a51863081e2ab07e36ddfeaab
Tim Jenness <tjenness@lsst.org>
2019-01-18 10:51:10 -0700

    Add a note about astro_metadata_translator coding standards

--- python/lsst/obs/lsst/translators/__init__.py
+++ python/lsst/obs/lsst/translators/__init__.py
@@ -1,3 +1,10 @@
+# This directory contains the translator plugins for the
+# astro_metadata_translator package.  The plugins follow the coding
+# style of astro_metadata_translator rather than the coding style
+# of obs_lsst.  The intent is for these plugins to be transferrable
+# out of this package and integrated into astro_metadata_translator when
+# they become stable.
+
 from .phosim import *
 from .ts8 import *
 from .imsim import *
