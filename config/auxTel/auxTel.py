08564989242322643e9b2008253f7bffa8e51da8
Robert Lupton the Good <rhl@astro.princeton.edu>
2019-01-24 14:42:00 -0500

    ip_isr now raises an error if no crosstalk coefficients are available

--- config/auxTel/auxTel.py
+++ config/auxTel/auxTel.py
@@ -28,5 +28,5 @@ if hasattr(config, 'ccdKeys'):
 
 config.isr.doLinearize = False
 config.isr.doDefect = False
-config.isr.doCrosstalk=True
+config.isr.doCrosstalk=False
 config.isr.doAddDistortionModel = False
---
+++ config/ts8/runIsr.py
@@ -0,0 +1,4 @@
+import os.path
+from lsst.utils import getPackageDir
+
+config.load(os.path.join(getPackageDir("obs_lsst"), "config", "ts8", "ts8.py"))
