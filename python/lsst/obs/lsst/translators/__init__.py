85d55575066aa72cb9bb148f8406cae6d9b598b1
Tim Jenness <tjenness@lsst.org>
2019-01-25 11:29:31 -0700

    Make it clear that the translator code is special
    It is intended to move it later.

--- python/lsst/obs/lsst/translators/__init__.py
+++ python/lsst/obs/lsst/translators/__init__.py
@@ -1,9 +1,12 @@
-# This directory contains the translator plugins for the
-# astro_metadata_translator package.  The plugins follow the coding
-# style of astro_metadata_translator rather than the coding style
-# of obs_lsst.  The intent is for these plugins to be transferrable
-# out of this package and integrated into astro_metadata_translator when
-# they become stable.
+# This file is currently part of obs_lsst but is written to allow it
+# to be migrated to the astro_metadata_translator package at a later date.
+#
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the LICENSE file in this directory for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
 
 from .phosim import *
 from .ts8 import *
--- python/lsst/obs/lsst/translators/lsst.py
+++ python/lsst/obs/lsst/translators/lsst.py
@@ -1,23 +1,12 @@
-# This file is part of obs_lsst.
+# This file is currently part of obs_lsst but is written to allow it
+# to be migrated to the astro_metadata_translator package at a later date.
 #
-# Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
 # (http://www.lsst.org).
-# See the COPYRIGHT file at the top-level directory of this distribution
-# for details of code ownership.
+# See the LICENSE file in this directory for details of code ownership.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
 
 """Metadata translation support code for LSST headers"""
 
