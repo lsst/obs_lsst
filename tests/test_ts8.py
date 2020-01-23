c2bc2404156c8f4b3d29ef1bce6c92379fc93161
Simon Krughoff <simon.krughoff@gmail.com>
2020-01-23 18:19:47 +0000

    Add RTM-018 to camera description

--- tests/test_ts8.py
+++ tests/test_ts8.py
@@ -109,7 +109,7 @@ class TestTs8(ObsLsstObsBaseOverrides, ObsLsstButlerTests):
                           )
 
         self.setUp_camera(camera_name='lsstCam',
-                          n_detectors=216,
+                          n_detectors=225,
                           first_detector_name='RTM-002_S00',
                           plate_scale=20.0 * arcseconds,
                           )
