bd886764a6694cf7b53d8fc5db675ae9061eb831
Paul Price <price@astro.princeton.edu>
2020-01-31 12:54:23 -0500

    config: update parameters follow deprecation
    doApplyExternal* and doApplySkyCorr moved down a level,
    into the ReadCalibratedExposureTask.

--- config/makeCoaddTempExp.py
+++ config/makeCoaddTempExp.py
@@ -31,8 +31,8 @@ config.warpAndPsfMatch.psfMatch.kernel['AL'].alardSigGauss = [1.0, 2.0, 4.5]
 config.modelPsf.defaultFwhm = 7.7
 
 # FUTURE: Set both to True when we decide to run jointcal
-config.doApplyExternalPhotoCalib = False
-config.doApplyExternalSkyWcs = False
+config.readCalexp.doApplyExternalPhotoCalib = False
+config.readCalexp.doApplyExternalSkyWcs = False
 
 # FUTURE: Set to True when we have sky background estimate
-config.doApplySkyCorr = False
+config.readCalexp.doApplySkyCorr = False
