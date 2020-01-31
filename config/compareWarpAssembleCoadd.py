6f164af4ec170baea927609a6b27a3e285ea1aa9
Paul Price <price@astro.princeton.edu>
2020-01-31 12:55:27 -0500

    config: remove useless overrides
    The doApplyExternal* don't affect the operation of coaddition,
    and they've been deprecated.

--- config/compareWarpAssembleCoadd.py
+++ config/compareWarpAssembleCoadd.py
@@ -28,7 +28,3 @@ config.load(os.path.join(getPackageDir("obs_lsst"), "config", "assembleCoadd.py"
 
 # 200 rows (since patch width is typically < 10k pixels
 config.assembleStaticSkyModel.subregionSize = (10000, 200)
-
-# FUTURE Set both to True if we run meas_mosaic or jointcal
-config.assembleStaticSkyModel.doApplyExternalPhotoCalib = False
-config.assembleStaticSkyModel.doApplyExternalSkyWcs = False
