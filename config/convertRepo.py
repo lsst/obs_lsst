6dcdde144ec298c451a91be122009a012f0700d1
Tim Jenness <tjenness@lsst.org>
2020-03-17 14:19:34 -0700

    Add gen2to3 conversion config

---
+++ config/convertRepo.py
@@ -0,0 +1,10 @@
+# Config overrides for converting gen2 to gen3 repos.
+
+# For LSST data the raw file is associated with the raw_amp dataset type
+config.rawDatasetType = "raw_amp"
+
+# LSST names its detectors differently in the registry
+config.ccdKey = "detector"
+
+# Ignore these special dataset types
+config.datasetIgnorePatterns.extend(["_raw", "raw_hdu", "raw_amp"])
