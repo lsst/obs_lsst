b76e3b5365ab5f03f1ed34b1d4bba43962214695
Tim Jenness <tjenness@lsst.org>
2020-03-19 15:44:39 -0700

    Add gen2 to 3 repo conversion tests

---
+++ tests/config/convert2to3Config.py
@@ -0,0 +1,6 @@
+# Config overrides for `test_convert2to3.py`
+# These are designed to replace the `config/convertRepo.py` settings
+
+config.skyMaps = {}
+config.rootSkyMapName = None
+config.refCats = []
