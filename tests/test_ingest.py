cc4684f836954440647873bf762249ff2f4062d3
Tim Jenness <tjenness@lsst.org>
2020-03-22 14:57:31 -0700

    Fix test_ingest prologue so it does not say obs_decam

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,4 +1,4 @@
-# This file is part of obs_decam.
+# This file is part of obs_lsst.
 #
 # Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
