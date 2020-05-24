efaa6cbc1efa85e3a1d11552b8b872ff6458b51c
Lauren MacArthur <lauren@astro.princeton.edu>
2020-05-24 14:44:35 -0500

    Add processCcd.py config settings for latiss

--- config/latiss/processCcd.py
+++ config/latiss/processCcd.py
@@ -19,5 +19,36 @@
 # You should have received a copy of the LSST License Statement and
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
+import os.path
+
+obsConfigDir = os.path.dirname(__file__)
 
 config.isr.doCrosstalk=False
+
+# Use Gaia-DR2 for astrometric calibration, and Panstarrs-1 DR1 for photometric
+# calibration and for identifying stars for PSF measurements.
+config.calibrate.astromRefObjLoader.ref_dataset_name = "gaia_dr2_20200414"
+config.calibrate.astromRefObjLoader.load(os.path.join(obsConfigDir, "gaiaFilterMap.py"))
+
+config.calibrate.photoRefObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
+config.calibrate.photoRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+config.calibrate.photoCal.match.referenceSelection.magLimit.fluxField = "i_flux"
+config.charImage.refObjLoader.ref_dataset_name = config.calibrate.photoRefObjLoader.ref_dataset_name
+config.charImage.refObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+
+# gen3 consistency settings for the above
+config.calibrate.connections.astromRefCat = config.calibrate.astromRefObjLoader.ref_dataset_name
+config.calibrate.connections.photoRefCat = config.calibrate.photoRefObjLoader.ref_dataset_name
+
+# Demand astrometry and photoCal succeed
+config.calibrate.requireAstrometry = False
+config.calibrate.requirePhotoCal = False
+
+config.calibrate.photoCal.reserve.fraction = 0.0
+config.charImage.measurePsf.reserve.fraction = 0.0
+config.charImage.measurePsf.starSelector["objectSize"].doFluxLimit = True
+config.charImage.measurePsf.starSelector["objectSize"].fluxMin = 4000
+config.charImage.measurePsf.starSelector["objectSize"].doSignalToNoiseLimit = True
+
+config.charImage.measurement.plugins["base_Jacobian"].pixelScale = 0.1  # arcsec/pix
+config.calibrate.measurement.plugins["base_Jacobian"].pixelScale = 0.1  # arcsec/pix
