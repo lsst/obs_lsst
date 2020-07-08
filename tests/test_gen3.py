439e72a41cd48d8816d08b31dc0571c96f38efa6
Robert Gruendl <gruendl@illinois.edu>
2020-07-08 17:12:20 -0500

    Fix flake8 (again)

--- tests/test_gen3.py
+++ tests/test_gen3.py
@@ -131,11 +131,11 @@ class TestInstruments(unittest.TestCase):
 
     def testLsstCam(self):
         testFpath="lsstCam/raw/2019-03-22/3019032200002/3019032200002-R10-S22-det035.fits"
-        self.checkInstrumentWithRegistry(LsstCam,testFpath)
+        self.checkInstrumentWithRegistry(LsstCam, testFpath)
 
     def testComCam(self):
         testFpath="comCam/raw/2019-05-30/3019053000001/3019053000001-R22-S00-det000.fits"
-        self.checkInstrumentWithRegistry(LsstComCam,testFpath)
+        self.checkInstrumentWithRegistry(LsstComCam, testFpath)
 
     def testImSim(self):
         self.checkInstrumentWithRegistry(LsstImSim,
