90056f8180b25cd8e1496687935bd9e48b176a74
suberlak <suberlak@uw.edu>
2020-12-08 22:24:40 +0000

    changed P to H for pHosim

--- python/lsst/obs/lsst/translators/lsst.py
+++ python/lsst/obs/lsst/translators/lsst.py
@@ -398,12 +398,12 @@ class LsstBaseTranslator(FitsTranslator):
                 # Add 1000 to the year component
                 dayobs = int(dayobs)
                 dayobs += 1000_00_00
-            elif controller == "P":
+            elif controller == "H":
                 # Add 2000 to the year component
                 dayobs = int(dayobs)
                 dayobs += 2000_00_00
             else:
-                raise ValueError(f"Supplied controller, '{controller}' is neither 'O' nor 'C' nor 'P'")
+                raise ValueError(f"Supplied controller, '{controller}' is neither 'O' nor 'C' nor 'H'")
 
         # Form the number as a string zero padding the sequence number
         idstr = f"{dayobs}{seqnum:0{maxdigits}d}"
