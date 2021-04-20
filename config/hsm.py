58923029da644a4f8b7dfdd7b8a6876c51ce1e15
Josh Meyers <jmeyers314@gmail.com>
2021-04-20 13:15:03 -0500

    Enable debiased PSF moments

--- config/hsm.py
+++ config/hsm.py
@@ -20,7 +20,17 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 
+
+# Enable HSM shapes (unsetup meas_extensions_shapeHSM to disable)
+# 'config' is a SourceMeasurementConfig.
 import os.path
 from lsst.utils import getPackageDir
-config.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
-config.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
+from lsst.pex.exceptions.wrappers import NotFoundError
+
+try:
+    config.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
+    config.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
+    # Enable debiased moments
+    config.plugins.names |= ["ext_shapeHSM_HsmPsfMomentsDebiased"]
+except NotFoundError as e:
+    print("Cannot enable shapeHSM (%s): disabling HSM shape measurements" % (e,))
