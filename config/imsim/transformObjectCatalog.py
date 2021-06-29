ad59aecadfe1080f03661687e29713b71dff7514
Tim Jenness <tjenness@lsst.org>
2021-06-29 11:25:40 -0700

    Use environment variable in config to lazy load functor file location

--- config/imsim/transformObjectCatalog.py
+++ config/imsim/transformObjectCatalog.py
@@ -1,8 +1,9 @@
 import os
-from lsst.utils import getPackageDir
 
 # Always produce these output bands in the Object Tables,
 # regardless which bands were processed
 config.outputBands = ["u", "g", "r", "i", "z", "y"]
 
-config.functorFile = os.path.join(getPackageDir('obs_lsst'), 'policy', 'imsim', 'Object.yaml')
+# Use the environment variable to prevent hardcoding of paths
+# into quantum graphs.
+config.functorFile = os.path.join('$OBS_LSST_DIR', 'policy', 'imsim', 'Object.yaml')
