1e7788fba8d4e89b6c48dfac8203e5a67c4c3c28
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2021-11-25 10:22:43 -0600

    Further tweaking of PSF measurement

--- config/latiss/characterizeImage.py
+++ config/latiss/characterizeImage.py
@@ -13,5 +13,21 @@ config.refObjLoader.ref_dataset_name = REFCAT
 
 config.doDeblend = False
 
+# keep these
 config.measurePsf.psfDeterminer = 'pca'
 config.measurePsf.psfDeterminer['pca'].spatialOrder = 0
+config.measurePsf.starSelector['objectSize'].fluxMin = 12500./5
+config.installSimplePsf.width = 81  # why is the default 11?!
+config.installSimplePsf.fwhm = 2.355*2
+
+# playground
+# config.measurePsf.psfDeterminer['pca'].tolerance = 0
+# config.measurePsf.psfDeterminer['pca'].reducedChi2ForPsfCandidates = 0
+
+config.measurePsf.psfDeterminer['pca'].sizeCellX = 4100
+config.measurePsf.psfDeterminer['pca'].sizeCellY = 4100
+
+# Apparently pointless
+# config.measurePsf.starSelector['objectSize'].widthMax = 40.
+# config.measurePsf.starSelector['objectSize'].widthStdAllowed = .5
+# config.measurePsf.starSelector['objectSize'].nSigmaClip = 3
