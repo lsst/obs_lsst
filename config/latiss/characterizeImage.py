57b752471558d9f12ce8e1a984dabe7b7ebe5046
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2021-12-01 13:40:04 -0600

    Change to singel eigen component fitting, remove temp options

--- config/latiss/characterizeImage.py
+++ config/latiss/characterizeImage.py
@@ -20,12 +20,10 @@ config.measurePsf.starSelector['objectSize'].fluxMin = 12500./5
 config.installSimplePsf.width = 81  # why is the default 11?!
 config.installSimplePsf.fwhm = 2.355*2
 
-# playground
-# config.measurePsf.psfDeterminer['pca'].tolerance = 0
-# config.measurePsf.psfDeterminer['pca'].reducedChi2ForPsfCandidates = 0
+config.measurePsf.psfDeterminer['pca'].nEigenComponents = 1
 
-config.measurePsf.psfDeterminer['pca'].sizeCellX = 4100
-config.measurePsf.psfDeterminer['pca'].sizeCellY = 4100
+# playground
+
 
 # Apparently pointless
 # config.measurePsf.starSelector['objectSize'].widthMax = 40.
