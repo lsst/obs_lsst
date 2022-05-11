a86090d6bd291b55c788c2fd460a4cf9cdd7f6e3
edennihy <edennihy@gmail.com>
2022-05-11 13:03:41 -0500

    remove deprecated Gen2 refcat setting, use default catalog (gaia)

--- config/latiss/characterizeImage.py
+++ config/latiss/characterizeImage.py
@@ -1,19 +1,6 @@
 from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
 
-# Support multiple refacts with configuration at a single point here,
-# with logic following to allow easy switching between Gaia and Pan-STARRS
-REFCAT_NAME = 'gaia'
-# REFCAT_NAME = 'ps1'
-
-if REFCAT_NAME == 'gaia':
-    REFCAT = "gaia_dr2_20200414"
-else:
-    REFCAT = "ps1_pv3_3pi_20170110"
-
-#config.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
-config.refObjLoader.ref_dataset_name = REFCAT
-
-# Wwitch to PCA determiner as it is much more robust when there are very few
+# Switch to PCA determiner as it is much more robust when there are very few
 # sources. Increase default FWHM due to plate scale and reduce fluxMin due to
 # short expTimes on a 1.2m telescope. nEigenComponents and spatialOrder set to
 # min values so that we always succeed even with one or two stars.
