4ccb97a04a6d12d5f611cac40a4c46231d114f91
John Parejko <parejkoj@uw.edu>
2022-05-10 17:12:49 -0700

    Add HSC-specific warning

--- config/apertures.py
+++ config/apertures.py
@@ -25,6 +25,7 @@
 # 'config' should be a SourceMeasurementConfig
 
 config.plugins.names |= ["base_CircularApertureFlux"]
+# TODO: these numbers assume an HSC plate scale!
 # Roughly (1.0, 1.4, 2.0, 2.8, 4.0, 5.7, 8.0, 11.3, 16.0, 22.6 arcsec) in diameter: 2**(0.5*i)
 # (assuming plate scale of 0.168 arcsec pixels)
 config.plugins["base_CircularApertureFlux"].radii = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
