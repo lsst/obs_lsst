5208c0fb799b216bd6e88253e8047050817e6245
James Chiang <jchiang87@gmail.com>
2023-05-23 21:23:42 -0700

    we can simplify here since the subclasses handle the 'empty' and empty string filter keyword values

--- python/lsst/obs/lsst/translators/lsst.py
+++ python/lsst/obs/lsst/translators/lsst.py
@@ -666,14 +666,6 @@ class LsstBaseTranslator(FitsTranslator):
         if not joined:
             joined = "unknown"
 
-        # Remove blank and "empty" fields.
-        joined = FILTER_DELIMITER.join(_ for _ in joined.split(FILTER_DELIMITER)
-                                       if _ and _ != "empty")
-
-        # Return "empty" if joined is blank at this point.
-        if not joined:
-            joined = "empty"
-
         return joined
 
     @cache_translation
