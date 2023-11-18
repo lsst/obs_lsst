ede50ba5fe3fe4f2dfea0aea90bd9f44a24e7bbf
Eli Rykoff <erykoff@stanford.edu>
2023-11-18 11:46:23 -0800

    Fix fgcm lookup table config to map empty~i/y to i/y~empty.

--- config/latiss/fgcmMakeLut.py
+++ config/latiss/fgcmMakeLut.py
@@ -9,5 +9,14 @@ config.physicalFilters = [
     "empty~SDSSy_65mm",
 ]
 
+# Override mapping from physical filter labels to 'standard' physical
+# filter labels. The 'standard' physical filter defines the transmission
+# curve that the FGCM standard bandpass will be based on.
+# Any filter not listed here will be mapped to
+# itself.  These overrides say that empty~i and empty~y should
+# me mapped onto i~empty and y~empty.
+config.stdPhysicalFilterOverrideMap = {"empty~SDSSi_65mm": "SDSSi_65mm~empty",
+                                       "empty~SDSSy_65mm": "SDSSy_65mm~empty"}
+
 # FGCM name or filename of precomputed atmospheres
 config.atmosphereTableName = 'fgcm_atm_lsst2'
