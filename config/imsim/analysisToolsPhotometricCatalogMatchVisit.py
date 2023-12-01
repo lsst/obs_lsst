b20f7b87fa4a4468083e7020439fe4498bb42d7c
Yusra AlSayyad <yusra@astro.princeton.edu>
2023-11-30 18:49:22 -0800

    Add imsim specific overrides
    for analysis tools photometric matching tasks, because
    no colorterms apply. Pair coded hotfix with Sophie.

---
+++ config/imsim/analysisToolsPhotometricCatalogMatchVisit.py
@@ -0,0 +1,5 @@
+import os.path
+configDir = os.path.dirname(__file__)
+config.referenceCatalogLoader.refObjLoader.load(os.path.join(configDir, 'filterMap.py'))
+
+config.referenceCatalogLoader.doApplyColorTerms = False
