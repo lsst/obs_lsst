eec73905d120398f070a935417b47ab2c0744281
Lauren MacArthur <lauren@astro.princeton.edu>
2024-03-04 15:19:28 -0800

    Set mag limits for refcat loading in LSSTComCamSim
    These have been selected to more than span the expected magnitude
    range for the icSrc catalog sources (those available for the
    calibrations).  Loading in reference sources outside of this range
    is undesirable for a few reasons, including wasting time on objects
    that are not acutally in contention for matcing and, more importantly,
    leaving a danger of the fit getting "lost" by locking onto the
    "superfluous" reference catalog objects (we saw this happen in
    photoCal for 3 of 25935 dataIds in the first round Ops Rehearsal 3
    dataset).

--- config/comCamSim/calibrateImage.py
+++ config/comCamSim/calibrateImage.py
@@ -10,3 +10,21 @@ config.astrometry_ref_loader.load(os.path.join(configDir, "filterMap.py"))
 config.photometry_ref_loader.load(os.path.join(configDir, "filterMap.py"))
 config.astrometry_ref_loader.anyFilterMapsToThis = None
 config.photometry_ref_loader.anyFilterMapsToThis = None
+
+# The following magnitude limits for reference objects used in the
+# astrometric and photometric calibrations were selected to more than
+# span the expected magnitude range of the science sources available
+# for the calibrations (see DM-43143 for example distributions).  This
+# is to avoid passing reference objects to the matcher that sould not
+# be in contention for matching (thus reducing the chance of locking
+# onto a bad match).
+# TODO: remove (or update) once DM-43168 is implemented.
+config.astrometry.referenceSelector.doMagLimit = True
+config.astrometry.referenceSelector.magLimit.fluxField = "lsst_r_flux"
+config.astrometry.referenceSelector.magLimit.minimum = 14.0
+config.astrometry.referenceSelector.magLimit.maximum = 22.0
+
+config.photometry.match.referenceSelection.doMagLimit = True
+config.photometry.match.referenceSelection.magLimit.fluxField = "lsst_r_flux"
+config.photometry.match.referenceSelection.magLimit.minimum = 14.0
+config.photometry.match.referenceSelection.magLimit.maximum = 22.0
