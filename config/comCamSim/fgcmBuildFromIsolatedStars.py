1cb875005f16d7568ee5999a02353e589cdbb209
Eli Rykoff <erykoff@stanford.edu>
2024-03-06 10:09:38 -0800

    Add configuration for fgcmcal star building for comCamSim.

---
+++ config/comCamSim/fgcmBuildFromIsolatedStars.py
@@ -0,0 +1,33 @@
+import os
+
+physical_to_band = {
+    "g_01": "g",
+    "r_03": "r",
+    "i_06": "i",
+}
+
+config.requiredBands = []
+config.primaryBands = ["i", "r", "g"]
+
+config.coarseNside = 64
+
+config.minPerBand = 2
+config.connections.ref_cat = "uw_stars_20240130"
+
+configDir = os.path.join(os.path.dirname(__file__))
+config.physicalFilterMap = physical_to_band
+config.doSubtractLocalBackground = True
+config.sourceSelector["science"].flags.bad.append("localBackground_flag")
+config.fgcmLoadReferenceCatalog.filterMap = {
+    "g": "lsst_g",
+    "r": "lsst_r",
+    "i": "lsst_i",
+    "g_01": "lsst_g",
+    "r_03": "lsst_r",
+    "i_06": "lsst_i",
+}
+config.fgcmLoadReferenceCatalog.applyColorTerms = False
+config.fgcmLoadReferenceCatalog.referenceSelector.doSignalToNoise = True
+config.fgcmLoadReferenceCatalog.referenceSelector.signalToNoise.fluxField = "lsst_i_flux"
+config.fgcmLoadReferenceCatalog.referenceSelector.signalToNoise.errField = "lsst_i_fluxErr"
+config.fgcmLoadReferenceCatalog.referenceSelector.signalToNoise.minimum = 50.0
