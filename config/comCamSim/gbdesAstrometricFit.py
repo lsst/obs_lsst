29e9374707e227158d466903e747505af5c89ba8
Clare Saunders <cmsaunders@princeton.edu>
2024-03-06 13:42:21 -0800

    Add configs for gbdes

--- config/comCamSim/gbdesAstrometricFit.py
+++ config/comCamSim/gbdesAstrometricFit.py
@@ -2,3 +2,7 @@
 comCamSim-specific overrides for GbdesAstrometricFitTask
 """
 config.connections.referenceCatalog = "uw_stars_20240228"
+config.applyRefCatProperMotion = False
+config.referenceFilter = 'lsst_r'
+config.devicePolyOrder = 5
+config.exposurePolyOrder = 7
