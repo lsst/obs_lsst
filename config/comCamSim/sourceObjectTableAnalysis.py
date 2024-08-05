f8d1577198800f41c556e477644e6df4086f7ad1
Clare Saunders <cmsaunders@princeton.edu>
2024-08-05 08:02:58 -0700

    Turn off proper motion correction

---
+++ config/comCamSim/sourceObjectTableAnalysis.py
@@ -0,0 +1,3 @@
+# Proper motion is not fit in gbdesAstrometricFitTask because proper motion was
+# not included in the simulations. Thus it cannot be applied here.
+config.applyAstrometricCorrections = False
---
+++ config/imsim/sourceObjectTableAnalysis.py
@@ -0,0 +1,3 @@
+# gbdesAstrometricFitTask is not run for imsim, so there is no proper motion
+# catalog to use here.
+config.applyAstrometricCorrections = False
