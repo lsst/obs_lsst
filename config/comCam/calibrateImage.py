35c61bc1c68e267ea0563491384ec5e889523daa
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-08-07 12:07:42 -0700

    Add calibrateImage.py for ComCam and LSSTCam

---
+++ config/comCam/calibrateImage.py
@@ -0,0 +1,15 @@
+# Configure the photometry to use atlas_refcat2.
+config_dir = os.path.dirname(__file__)
+
+config.connections.photometry_ref_cat = "atlas_refcat2_20220201"
+config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+
+config.photometry.match.referenceSelection.magLimit.fluxField = "r_flux"
+colors = config.photometry.match.referenceSelection.colorLimits
+colors["g-r"] = ColorLimit(primary="g_flux", secondary="r_flux", minimum=0.4, maximum=2.0)
+
+config.photometry.applyColorTerms = True
+config.photometry.photoCatName = "atlas_refcat2_20220201"
+config.photometry.colorterms.load(os.path.join(config_dir, "colorterms.py"))
+
+config.compute_summary_stats.load(os.path.join(configDir, "computeExposureSummaryStats.py"))
