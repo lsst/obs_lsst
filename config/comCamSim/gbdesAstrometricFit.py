0eae31b410935685cc48ea17d58dd362afa133dc
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-08-14 12:22:22 -0700

    Do not fit proper motion for ComCamSim

--- config/comCamSim/gbdesAstrometricFit.py
+++ config/comCamSim/gbdesAstrometricFit.py
@@ -3,6 +3,7 @@ comCamSim-specific overrides for GbdesAstrometricFitTask
 """
 config.connections.referenceCatalog = "uw_stars_20240524"
 config.applyRefCatProperMotion = False
+config.fitProperMotion = False
 config.referenceFilter = "lsst_r"
 config.devicePolyOrder = 5
 config.exposurePolyOrder = 7
