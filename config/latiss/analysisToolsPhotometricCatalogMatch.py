eb1715352cf7b687d80a0ce8d8279f5b425bb9d7
psferguson <petersferguson@gmail.com>
2024-10-16 14:01:25 -0700

    configs for coaddQualityCore

--- config/latiss/analysisToolsPhotometricCatalogMatch.py
+++ config/latiss/analysisToolsPhotometricCatalogMatch.py
@@ -4,6 +4,7 @@ OBS_CONFIG_DIR = os.path.dirname(__file__)
 
 config.referenceCatalogLoader.refObjLoader.load(os.path.join(OBS_CONFIG_DIR, "filterMap.py"))
 config.referenceCatalogLoader.doApplyColorTerms = False
+config.connections.refCatalog =  "the_monster_20240904"
 
 config.filterNames = ["SDSSg_65mm~empty",
                       "SDSSr_65mm~empty",
