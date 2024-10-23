f576cbaf8b6a6f3eaa5d12f0465a77d763ec6d75
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-10-23 12:08:34 -0700

    Switch to The Monster for RefCatSourcePhotometricAnalysisTask

--- config/refCatSourcePhotometricAnalysisTask.py
+++ config/refCatSourcePhotometricAnalysisTask.py
@@ -1,2 +1,3 @@
 config.connections.refCatalog = "the_monster_20240904"
+config.connections.data = "sourceTable_visit_the_monster_20240904_match_photom"
 config.connections.outputName = "sourceTable_visit_the_monster_20240904_match_photom"
