ee15543ed6f20b3ef398e5b3da9467b5cf82d0a5
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2024-10-23 12:05:38 -0700

    Switch to The Monster as default refcat for base calibrate config
    Photometry only, still need Gaia for astrom for now.

--- config/calibrate.py
+++ config/calibrate.py
@@ -29,7 +29,7 @@ obsConfigDir = os.path.dirname(__file__)
 
 # TODO: Turn color terms back on when they are available
 config.photoCal.applyColorTerms = False
-config.photoCal.photoCatName = "atlas_refcat2_20220201"
+config.photoCal.photoCatName = "the_monster_20240904"
 
 # Activate calibration of measurements: required for aperture corrections
 config.measurement.load(os.path.join(obsConfigDir, "apertures.py"))
@@ -41,4 +41,4 @@ config.measurement.plugins.names |= ["base_Jacobian", "base_FPPosition"]
 config.measurement.plugins["base_Jacobian"].pixelScale = 0.2
 
 config.connections.astromRefCat = "gaia_dr3_20230707"
-config.connections.photoRefCat = "atlas_refcat2_20220201"
+config.connections.photoRefCat = "the_monster_20240904"
