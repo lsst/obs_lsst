8a6286b8d98e89955c6f50118c0fa3090aa73197
Jim Bosch <jbosch@astro.princeton.edu>
2024-10-24 11:49:15 -0400

    Drop unnecessary analysis_tools output connection override.
    The suffix on this override was incorrect (_photoMatch instead of
    _match_photom), and just dropping the override seems to fix it,
    presumably because of some magic in analysis_tools.

--- config/comCamSim/analysisToolsPhotometricCatalogMatch.py
+++ config/comCamSim/analysisToolsPhotometricCatalogMatch.py
@@ -7,5 +7,4 @@ config.referenceCatalogLoader.refObjLoader.load(os.path.join(configDir, "filterM
 config.referenceCatalogLoader.doApplyColorTerms = False
 
 config.connections.refCatalog = "uw_stars_20240524"
-config.connections.matchedCatalog = "objectTable_tract_uw_stars_20240524_photoMatch"
 config.filterNames = ("g", "r", "i")
