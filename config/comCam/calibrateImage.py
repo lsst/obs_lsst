f315699e87f34e039c859e9845b58a5f43b8b602
Yusra AlSayyad <yusra@astro.princeton.edu>
2024-11-07 07:17:41 -0800

    Increase matcher buffer to 1500pix

--- config/comCam/calibrateImage.py
+++ config/comCam/calibrateImage.py
@@ -24,8 +24,8 @@ config.photometry.photoCatName = "the_monster_20240904"
 config.compute_summary_stats.load(os.path.join(config_dir, "computeExposureSummaryStats.py"))
 
 # Loosen maxOffset to account for early pointing model inaccuracy.
-config.astrometry.matcher.maxOffsetPix=800
-config.astrometry_ref_loader.pixelMargin=800
+config.astrometry.matcher.maxOffsetPix=1500
+config.astrometry_ref_loader.pixelMargin=1500
 
 # Loosened for early ComCam with large PSFs.
 config.photometry.match.sourceSelection.doUnresolved = False
