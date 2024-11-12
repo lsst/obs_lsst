eab7774e0b258a25c9a61352fb6cab57ea2d435a
Eli Rykoff <erykoff@stanford.edu>
2024-11-11 16:11:21 -0800

    Add configuration for LSSTComCam fgcm output task.

---
+++ config/comCam/fgcmOutputProducts.py
@@ -0,0 +1,12 @@
+config.connections.cycleNumber = 6
+
+physical_to_band = {
+    "u_02": "u",
+    "g_01": "g",
+    "r_03": "r",
+    "i_06": "i",
+    "z_03": "z",
+    "y_04": "y",
+}
+
+config.physicalFilterMap = physical_to_band
