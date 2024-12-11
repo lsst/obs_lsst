0e0f50131223a70c135ad5e07bbacd709175ae05
Pierre-Francois Leget <leget@astro.princeton.edu>
2024-12-11 12:57:09 -0800

    Increase polynomial order in Piff from 3 to 4 for LSSTComCam

--- config/comCam/finalize_characterization.py
+++ config/comCam/finalize_characterization.py
@@ -1,4 +1,4 @@
-# DM-48026: Order 2 does not looks to be enough
-# for spatial interpolation of the PSF. Order 3
+# DM-48093: Order 2 does not looks to be enough
+# for spatial interpolation of the PSF. Order 4
 # remove most of spatial structure.
-config.psf_determiner['piff'].spatialOrder = 3
+config.psf_determiner['piff'].spatialOrder = 4
