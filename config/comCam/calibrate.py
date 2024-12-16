12753fdadba1a7bcb8c2025c8bd8898aa74d2dbd
Lauren MacArthur <lauren@astro.princeton.edu>
2024-12-16 12:14:44 -0800

    Increase detection threshold for astrometry
    This helps greatly in the more crowded fields and seems to do no harm
    otherwise.

--- config/comCam/calibrate.py
+++ config/comCam/calibrate.py
@@ -3,14 +3,20 @@ import os
 obsConfigDir = os.path.join(os.path.dirname(__file__))
 config.photoRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
 config.connections.astromRefCat = "the_monster_20240904"
+# config.astromRefObjLoader.load(os.path.join(obsConfigDir, "filterMap.py"))
+# config.astromRefObjLoader.anyFilterMapsToThis = None
 
 # Loosen maxOffset to account for early pointing model inaccuracy.
 config.astrometry.matcher.maxOffsetPix=1500
 config.astromRefObjLoader.pixelMargin=1500
 
+config.astrometry.sourceSelector["science"].signalToNoise.minimum = 20.0
+
 # Overrides to improved astrometry matching.
 config.astrometry.doFiducialZeroPointCull = True
 config.astrometry.load(os.path.join(obsConfigDir, "fiducialZeroPoint.py"))
+
+config.detection.thresholdValue = 25.0
 
 # Loosened for early ComCam with large PSFs.
 config.photoCal.match.sourceSelection.doUnresolved = False
