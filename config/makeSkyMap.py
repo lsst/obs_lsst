a706d38ab9f07f94ec5a8eeb34017fb308220d6e
jrmullaney <j.mullaney@sheffield.ac.uk>
2025-01-20 06:05:06 -0800

    Add tractbuilder.name="cells" to makeSkyMap.py

--- config/makeSkyMap.py
+++ config/makeSkyMap.py
@@ -25,3 +25,4 @@ config.skyMap["rings"].numRings = 120
 config.skyMap["rings"].projection = "TAN"
 config.skyMap["rings"].tractOverlap = 1.0 / 60  # Overlap between tracts (degrees)
 config.skyMap["rings"].pixelScale = 0.2
+config.skyMap["rings"].tractBuilder.name = "cells"
