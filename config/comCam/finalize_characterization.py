0a0388e86a0bef11f406594dbc3ba509c4b05d57
Pierre-Francois Leget <leget@astro.princeton.edu>
2025-02-14 07:46:58 -0800

    Enable zeroth order interp if not enough stars.

--- config/comCam/finalize_characterization.py
+++ config/comCam/finalize_characterization.py
@@ -2,3 +2,5 @@
 # for spatial interpolation of the PSF. Order 4
 # remove most of spatial structure.
 config.psf_determiner['piff'].spatialOrder = 4
+# See DM-48887 for more detail.
+config.psf_determiner['piff'].zerothOrderInterpNotEnoughStars = True
