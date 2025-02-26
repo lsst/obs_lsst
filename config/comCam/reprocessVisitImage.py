d567ee797ea99dd2457e561a633b98b143086d3c
Eli Rykoff <erykoff@stanford.edu>
2025-02-26 09:58:30 -0800

    Turn on illumination corrections for LSSTComCam.

--- config/comCam/reprocessVisitImage.py
+++ config/comCam/reprocessVisitImage.py
@@ -4,3 +4,5 @@ import os.path
 ObsConfigDir = os.path.dirname(__file__)
 
 config.compute_summary_stats.load(os.path.join(ObsConfigDir, "computeExposureSummaryStats.py"))
+
+config.do_apply_flat_background_ratio = True
