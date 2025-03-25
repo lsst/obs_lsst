330643487f1ef013737b92abf8bfb7a33b1f9b15
Pierre-Francois Leget <leget@astro.princeton.edu>
2025-03-25 12:00:59 -0700

    Disable zeroth order polynomial interp failure mode.

--- config/comCam/finalize_characterization.py
+++ config/comCam/finalize_characterization.py
@@ -1,4 +1,7 @@
-# See DM-49673 for more detail.
+# DM-48093: Order 2 does not looks to be enough
+# for spatial interpolation of the PSF. Order 4
+# remove most of spatial structure.
+# See DM-49673 for u-band.
 config.psf_determiner['piff'].spatialOrderPerBand = {
     "u": 2,
     "g": 4,
@@ -7,8 +10,8 @@ config.psf_determiner['piff'].spatialOrderPerBand = {
     "z": 4,
     "y": 4,
 }
-# See DM-48887 for more detail.
-config.psf_determiner['piff'].zerothOrderInterpNotEnoughStars = True
+# See DM-48887 & DM-49688 for more detail.
+config.psf_determiner['piff'].zerothOrderInterpNotEnoughStars = False
 # See DM-49152 for more detail.
 config.psf_determiner['piff'].piffBasisPolynomialSolver = "cpp"
 config.psf_determiner['piff'].piffPixelGridFitCenter = False
