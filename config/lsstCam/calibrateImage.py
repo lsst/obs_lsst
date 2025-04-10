364b87877270f702a52b938d1bce2987d65892a7
Eli Rykoff <erykoff@stanford.edu>
2025-04-10 10:41:25 -0700

    Update initial LSSTCam calibrateImage config for LSSTCam filters/monster.

--- config/lsstCam/calibrateImage.py
+++ config/lsstCam/calibrateImage.py
@@ -1,10 +1,34 @@
 import os.path
 
+from lsst.meas.algorithms import ColorLimit  # Configure the photometry to use the_monster.
+
 config_dir = os.path.dirname(__file__)
+
+config.connections.photometry_ref_cat = "the_monster_20250219"
+config.connections.astrometry_ref_cat = "the_monster_20250219"
+
+config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+
+config.photometry.match.referenceSelection.magLimit.fluxField = "monster_ComCam_r_flux"
+colors = config.photometry.match.referenceSelection.colorLimits
+colors["g-r"] = ColorLimit(
+    primary="monster_ComCam_g_flux",
+    secondary="monster_ComCam_r_flux",
+    minimum=0.4,
+    maximum=2.0
+)
 
 config.photometry.applyColorTerms = False
 config.photometry.photoCatName = "the_monster_20250219"
-config.connections.photometry_ref_cat = "the_monster_20250219"
-config.connections.astrometry_ref_cat = "the_monster_20250219"
-config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
+
 config.compute_summary_stats.load(os.path.join(config_dir, "computeExposureSummaryStats.py"))
+
+# Loosen minMatchDistanceArcSec, the match distance below which further
+# iteration is pointless (arcsec) since we can only do as well as the
+# camera model in SFM using the affine WCS fit.
+# TODO: DM-47250. Tighten up (or remove) this override when an updated
+# camera model is implemented.
+config.astrometry.minMatchDistanceArcSec = 0.04
+
+# Loosened for early LSSTCam with large PSFs.
+config.photometry.match.sourceSelection.doUnresolved = False
