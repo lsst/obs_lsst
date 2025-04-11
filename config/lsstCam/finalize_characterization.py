58d77713f3f2c11d33ce2ddc1f953c178ca5849e
Yusra AlSayyad <yusra@astro.princeton.edu>
2025-04-11 16:04:59 -0700

    Start PIFF at spatial order 2 on LSSTCam

--- config/lsstCam/finalize_characterization.py
+++ config/lsstCam/finalize_characterization.py
@@ -1,14 +1,11 @@
-# DM-48093: Order 2 does not looks to be enough
-# for spatial interpolation of the PSF. Order 4
-# remove most of spatial structure.
-# See DM-49673 for u-band.
+# PFL recommends starting with spatial order 2
 config.psf_determiner['piff'].spatialOrderPerBand = {
     "u": 2,
-    "g": 4,
-    "r": 4,
-    "i": 4,
-    "z": 4,
-    "y": 4,
+    "g": 2,
+    "r": 2,
+    "i": 2,
+    "z": 2,
+    "y": 2,
 }
 # See DM-48887 & DM-49688 for more detail.
 config.psf_determiner['piff'].zerothOrderInterpNotEnoughStars = False
