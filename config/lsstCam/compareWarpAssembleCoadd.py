a14eeed9a6c52aff14cec526fabe9a1d7e475bc7
Yusra AlSayyad <yusra@astro.princeton.edu>
2025-05-01 21:11:05 -0700

    Increase threshold for clipping from 3 to 20%
    Preliminary adjustment based a small number of
    case studies from the nightly validation coadds
    between 20250415 and 20250429.

--- config/lsstCam/compareWarpAssembleCoadd.py
+++ config/lsstCam/compareWarpAssembleCoadd.py
@@ -1,6 +1,15 @@
 """
-comCamSim-specific overrides of CompareWarpAssembleCoaddTask
+LSSTCam-specific overrides of CompareWarpAssembleCoaddTask
 """
 
 config.subregionSize = (10000, 100)
 config.assembleStaticSkyModel.subregionSize = (10000, 100)
+
+# Preliminary aggressive adjustment to accomodate early commissioning cadence
+# increase the threshold for transient vs static designation from 3% to 20%
+config.maxFractionEpochsHigh=0.2
+
+# Shrink regions protected from clipping conservatively
+# This config protects the PSFs
+config.detectTemplate.nSigmaToGrow=1.2
+config.detectTemplate.thresholdValue=100
