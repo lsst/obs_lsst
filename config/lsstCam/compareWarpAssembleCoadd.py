ba5bf0365284b52a57ad036842f20e6c34511ab0
Yusra AlSayyad <yusra@astro.princeton.edu>
2025-05-21 15:25:34 -0700

    Increase maxFractionEpochsHigh during commissioning
    assembleDeepCoadd and assembleTemplateCoadd should have same configs
    unless otherwise specified. Set at 20% to start during commissioning
    based on data taken between April 20-May 5, in which each field
    is only observed for a few nights, but many observations in
    succession.

--- config/lsstCam/compareWarpAssembleCoadd.py
+++ config/lsstCam/compareWarpAssembleCoadd.py
@@ -1,6 +1,10 @@
 """
-comCamSim-specific overrides of CompareWarpAssembleCoaddTask
+lsstCam-specific overrides of CompareWarpAssembleCoaddTask
 """
 
 config.subregionSize = (10000, 100)
 config.assembleStaticSkyModel.subregionSize = (10000, 100)
+# Preliminary aggressive adjustment to accomodate early commissioning
+# deep-drilling-style cadence with many observations
+# in rapid sucessions across only few nights.
+config.maxFractionEpochsHigh = 0.2
