56fcf2da1d4f8087973083c273077943b9a82ec6
Lauren MacArthur <lauren@astro.princeton.edu>
2025-08-14 12:49:18 -0700

    Update LSSTCam astrometry matching configs
    The LSSTCam pointing model has improved significantly since setting
    the previous values.  Leaving them this large is ineffecient and often
    leads to matching difficulties in crowded fields.
    
    It also seems that the value for the maximum number of reference stars
    of 2**13 = 8192 is always overkill, so reducing to 4096.

--- config/lsstCam/calibrateImage.py
+++ config/lsstCam/calibrateImage.py
@@ -7,6 +7,7 @@ config_dir = os.path.dirname(__file__)
 config.connections.photometry_ref_cat = "the_monster_20250219"
 config.connections.astrometry_ref_cat = "the_monster_20250219"
 
+config.astrometry.load(os.path.join(config_dir, "filterMap.py"))
 config.photometry_ref_loader.load(os.path.join(config_dir, "filterMap.py"))
 
 config.photometry.match.referenceSelection.magLimit.fluxField = "monster_ComCam_r_flux"
@@ -23,16 +24,27 @@ config.photometry.photoCatName = "the_monster_20250219"
 
 config.compute_summary_stats.load(os.path.join(config_dir, "computeExposureSummaryStats.py"))
 
+# Decrease maximum number of reference sources
+config.astrometry.matcher.maxRefObjects = 4096
+
 # Loosen maxOffset to account for early pointing model inaccuracy.
-config.astrometry.matcher.maxOffsetPix=1500
-config.astrometry_ref_loader.pixelMargin=1500
+# NOTE: these only work for data post April 25, 2025, when the
+#       LSSTCam pointing model was dramatically improved.  Data
+#       taken before the update still require these to be ~1500 to
+#       obtain good astrometric fits, and the following ticket is
+#       to implement an acutal "fix" via the metadata-translator.
+# TODO: DM-53164: Refit the pointing for LSSTCam observations prior
+#       to April 25 and file metadata-patch tickets.
+config.astrometry.matcher.maxOffsetPix = 500
+config.astrometry_ref_loader.pixelMargin = 500
 
 # Loosen minMatchDistanceArcSec, the match distance below which further
 # iteration is pointless (arcsec) since we can only do as well as the
-# camera model in SFM using the affine WCS fit.
+# camera model in SFM using the affine WCS fit.  (Having a larger value
+# also gives poor seeing data a better chance of passing.)
 # TODO: DM-47250. Tighten up (or remove) this override when an updated
 # camera model is implemented.
-config.astrometry.minMatchDistanceArcSec = 0.04
+config.astrometry.minMatchDistanceArcSec = 0.07
 
 # Overrides to improved astrometry matching.
 config.astrometry.doFiducialZeroPointCull = True
