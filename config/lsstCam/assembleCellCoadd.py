3c9e8099041e5540cd9d56bd84e04b211d721112
Yusra AlSayyad <yusra@astro.princeton.edu>
2025-10-22 10:29:08 -0700

    Add PARTLY_VIGNETTED to LSSTCam bad mask planes
    to exclude in coadds.
    Add inline comment on why excluding VIGNETTED pixels is unnecessary
    VIGNETTED are also NO_DATA for all cameras, and NO_DATA
    is excluded by default. LSSTCam does not need different
    behavior or config specification

---
+++ config/lsstCam/assembleCellCoadd.py
@@ -0,0 +1,7 @@
+"""
+lsstCam-specific overrides for assembleCellCoadd
+"""
+
+# Need to reject VIGNETTED pixels, but VIGNETTED pixels also have NO_DATA set,
+# and NO_DATA is in default bad_mask_planes.
+config.bad_mask_planes += ["PARTLY_VIGNETTED"]
