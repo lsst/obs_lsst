758cabca577c7855cd2bb25520f1022b55653a8b
Ian Sullivan <sullii@uw.edu>
2025-10-22 20:27:47 -0700

    Reject diffraction spike mask in assembleCoadd

--- config/lsstCam/assembleCoadd.py
+++ config/lsstCam/assembleCoadd.py
@@ -5,3 +5,4 @@ lsstCam-specific overrides for assembleCoadd and subclasses
 # Need to reject VIGNETTED pixels, but VIGNETTED pixels also have NO_DATA set,
 # and NO_DATA is in default badMaskPlanes.
 config.badMaskPlanes += ["PARTLY_VIGNETTED"]
+config.badMaskPlanes += ["SPIKE"]
