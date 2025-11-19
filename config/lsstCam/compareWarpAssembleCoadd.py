f56d8c8404059185eef69978d9a619703a437f4d
Yusra AlSayyad <yusra@astro.princeton.edu>
2025-11-19 09:34:55 -0800

    Remove overrides to reduce subregionSize
    These weren't actually helping the situation.
    This wasn't the memory use that was scaling with
    number of visits

--- config/lsstCam/compareWarpAssembleCoadd.py
+++ config/lsstCam/compareWarpAssembleCoadd.py
@@ -6,8 +6,6 @@ import os.path
 # Load configs from base assembleCoadd
 config.load(os.path.join(os.path.dirname(__file__), "assembleCoadd.py"))
 
-config.subregionSize = (10000, 100)
-config.assembleStaticSkyModel.subregionSize = (10000, 100)
 # Preliminary aggressive adjustment to accomodate early commissioning
 # deep-drilling-style cadence with many observations
 # in rapid sucessions across only few nights.
