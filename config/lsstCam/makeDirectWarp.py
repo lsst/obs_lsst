e07599a7329708b78dd9523185e8b1f6e0739c32
Lauren MacArthur <lauren@astro.princeton.edu>
2025-12-15 16:05:38 -0800

    Add overrides for star ellipticity metrics
    These metrics are measured on the stars used in the PSF model.  The
    values selected here were conditioned on the w_2025_49 dp2 pilot run
    of DM-53545.

--- config/lsstCam/makeDirectWarp.py
+++ config/lsstCam/makeDirectWarp.py
@@ -10,5 +10,23 @@ config.select.maxPsfApFluxDelta = 1.6
 config.select.maxEllipResidual = 0.19
 config.select.maxScaledSizeScatter = 0.07
 config.select.maxPsfApCorrSigmaScaledDelta = 0.15
+config.select.maxStarEPerBand = {
+    "u": 0.2,
+    "g": 0.2,
+    "r": 0.2,
+    "i": 0.2,
+    "z": 0.2,
+    "y": 0.2,
+    "fallback": 0.2,
+}
+config.select.maxStarUnNormalizedEPerBand = {
+    "u": 2.3,
+    "g": 2.3,
+    "r": 2.3,
+    "i": 2.3,
+    "z": 2.3,
+    "y": 2.3,
+    "fallback": 2.3,
+}
 
 config.select.excludeDetectors = [0, 20, 27, 65, 123, 161, 168, 188]
