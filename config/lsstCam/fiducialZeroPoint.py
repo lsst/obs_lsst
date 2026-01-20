12a6e30c5ebb48e8ba1e0a415f0f2fcb01c7cfc5
Alex Drlica-Wagner <kadrlica@fnal.gov>
2026-01-19 21:30:50 -0800

    fiducial zeropoint offsets from commissioning

--- config/lsstCam/fiducialZeroPoint.py
+++ config/lsstCam/fiducialZeroPoint.py
@@ -1,12 +1,13 @@
 # Fiducial values derived from SMTN-002 (v2024-03-06), based on
 # syseng_throughput v1.9. See DM-47034 for more details.
+# Offsets derived from commissioning data (DM-53728).
 
 # Fiducial ZeroPoint in electrons for 1s exposure
 config.fiducialZeroPoint = {
-    "u": 26.52,
-    "g": 28.51,
-    "r": 28.36,
-    "i": 28.17,
-    "z": 27.78,
-    "y": 26.82,
+  "u": 26.52 + 0.03,
+  "g": 28.51 + 0.06,
+  "r": 28.36 + 0.14,
+  "i": 28.17 + 0.12,
+  "z": 27.78 + 0.12,
+  "y": 26.82 + 0.05,
 }
