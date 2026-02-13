eca298b7dd07b4f8f4f9d6fbda67597678fa5cd0
Lee Kelvin <lkelvin@astro.princeton.edu>
2026-02-13 07:27:53 -0800

    Fix incorrect ComCam astrom connection

--- config/comCam/refCatSourceAnalysisTask.py
+++ config/comCam/refCatSourceAnalysisTask.py
@@ -1,2 +1,2 @@
 config.connections.refCatalog = "the_monster_20250219"
-config.connections.outputName = "sourceTable_visit_the_monster_20250219_match_photom"
+config.connections.outputName = "sourceTable_visit_the_monster_20250219_match_astrom"
