b94c2831e78067f965505b2eafdf1be7e6862df1
jrmullaney <j.mullaney@sheffield.ac.uk>
2026-06-10 10:18:23 -0700

    Edit default masks for wholeTractMaskFraction
    Move CROSSTALK and SENSOR_EDGE from imsim to default.

--- config/imsim/wholeTractMaskFractionAnalysis.py
+++ config/imsim/wholeTractMaskFractionAnalysis.py
@@ -1,5 +1,3 @@
 config.maskPlanes.remove("PARTLY_VIGNETTED")
 config.maskPlanes.remove("SPIKE")
 config.maskPlanes.remove("NOT_DEBLENDED")
-config.maskPlanes += ["CROSSTALK"]
-config.maskPlanes += ["SENSOR_EDGE"]
